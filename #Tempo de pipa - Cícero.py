c70b166eba7ec8613a63636267d4f75fd7e05c34
Wender Araujo Santos <wenderaraujosantos@Wenders-MacBook.local>
2026-08-18 19:13:18 -0300

    AtividadesPython

---
+++ #Tempo de pipa - Cícero.py
@@ -0,0 +1,34 @@
+#Tempo de pipa - Cícero
+
+#Primeira estrofe
+print("Quando você vem ou não")
+print("O que você quer de mim")
+print("Deixo por aí")
+
+#Segunda estrofe   
+print("O que você tem")
+print("De onde você é")
+print("Pode me esquecer")
+print("Se você quiser")
+print("Ou se deixar chover")
+print("Se você vier")
+
+#Pre Chorus
+print("Eu vou te acompanhar de fitas")
+print("Te ajudo a decorar os dias")
+print("Te empresto minha neblina")
+
+#Pre Chorus
+print("Vamos nos espalhar sem linhas")
+print("Ver o mundo girar de cima")
+print("No tempo da preguiça")
+
+#Refrão
+print("Mas tudo bem")
+print("O dia vai raiar")
+print("Pra gente se inventar de novo")
+
+#Repete Refrão e encerra a música   (21 exatos)
+print("Mas tudo bem")
+print("O dia vai raiar")
+print("Pra gente se inventar de novo")
---
+++ Atividade1.py
@@ -0,0 +1,10 @@
+# Descubra o número: “Um número somado ao seu dobro resulta em 36.” Qual é esse número?
+# Um número somado ao seu dobro resulta em 36
+# x + 2x = 36
+# 3x = 36
+# x = 36/3
+# x = 12
+
+x = 12
+resultado = x + (2 * x)
+print("O número é {0}, e seu dobro é {1}. Somados, o resultado é {2}.".format(x, 2*x, resultado))
